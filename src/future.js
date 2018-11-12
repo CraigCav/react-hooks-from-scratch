df05a624570216bfbad713119cdc8f2fd9ad53a6
Craig Cavalier <craig.cavalier@ezcater.com>
2018-11-12 15:50:54 -0500

    rough implementation of useEffect

--- src/future.js
+++ src/future.js
@@ -7,26 +7,49 @@ let currentInstance = null;
 export const use = render => {
   return class extends React.Component {
     state = {};
+    componentDidMount() {
+      this.afterRender();
+    }
+    componentDidUpdate() {
+      this.afterRender();
+    }
+    componentWillUnmount() {
+      this.effects.forEach(({ create, destroy }) => {
+        if (destroy) destroy();
+      });
+    }
+    afterRender() {
+      // unlike the real React implementation, this runs synchronously
+      this.effects.forEach(({ create, destroy }) => {
+        if (destroy) destroy();
+        create();
+      });
+    }
     render() {
       currentInstance = this;
       currentInstance.callIndex = 0;
+      currentInstance.effects = [];
       return render(this.props);
     }
   };
 };
 
-export const useState = initialValue => {
+const nextStateCell = () => {
   // close over current component and index (so they don't change under us)
   const frame = currentInstance;
-
-  // close over current component and index (so they don't change under us)
-  const key = currentInstance.callIndex;
-
-  // destructure current state
-  const { state } = currentInstance;
+  const { state, callIndex } = currentInstance;
 
   // increment the pointer for the next `useState` call
   currentInstance.callIndex++;
+  return {
+    frame,
+    state,
+    key: callIndex
+  };
+};
+
+export const useState = initialValue => {
+  const { frame, state, key } = nextStateCell();
 
   return [
     // either the value in the current memory position, or the initial value
@@ -36,4 +59,23 @@ export const useState = initialValue => {
     // to component setState instead
     newValue => frame.setState({ [key]: newValue })
   ];
+};
+
+const inputsAreEqual = (next, prev) =>
+  next && next.every((d, i) => d === prev[i]);
+
+export const useEffect = (create, inputs = [create]) => {
+  const { frame, state, key } = nextStateCell();
+
+  const prevEffect = state[key];
+
+  if (prevEffect && inputsAreEqual(inputs, prevEffect.inputs)) return;
+
+  const effect = (state[key] = {
+    create,
+    inputs,
+    destroy: prevEffect ? prevEffect.destroy : null
+  });
+
+  frame.effects.push(effect);
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { use, useState } from "./future";
+import { use, useState, useEffect } from "./future";
 
 const Example = use(() => {
   const [count, setCount] = useState(0);
+
+  const [mousePosition, setMousePosition] = useState({ x: null, y: null });
+
+  const updateMousePosition = ev => {
+    setMousePosition({ x: ev.clientX, y: ev.clientY });
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", updateMousePosition);
+    return () => window.removeEventListener("mousemove", updateMousePosition);
+  }, []);
 
   return (
     <div>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>Click me</button>
+      <p>{JSON.stringify(mousePosition)}</p>
     </div>
   );
 });
